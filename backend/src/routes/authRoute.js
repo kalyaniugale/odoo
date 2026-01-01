2864fdb38b882b42c20f499fa24f7f073025cad8
AdityaGG81 <adityaghorpade81@gmail.com>
2026-01-02 00:47:29 +0530

    feat(auth): add Google OAuth login/signup with route protection

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,11 +1,29 @@
 import express from "express";
 import isAuth from "../middlewares/isAuth.js"
-import { login, logout, signup ,getMe} from "../controllers/authController.js";
+import passport from "passport";
+import { login, logout, signup ,getMe, googleSuccess} from "../controllers/authController.js";
 const authRouter=express.Router();
 
 authRouter.post("/signup",signup);
 authRouter.post("/login",login);
 authRouter.post("/logout",logout);
 authRouter.get("/me", isAuth, getMe)
+
+authRouter.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+authRouter.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    session: false,
+    failureRedirect: `${process.env.CLIENT_URL}/login?error=google_auth_failed`,
+  }),
+  googleSuccess
+);
+
 
 export default authRouter
